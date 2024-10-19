ae9d04ef6a14bab8569841c7421ba1d74193e0c6
KaueMiziara <kauemiziara@gmail.com>
2024-10-19 20:35:32 -0300

    test: add cipher function test cases

--- src/caesar.zig
+++ src/caesar.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const testing = std.testing;
 
 pub fn cipher(text: []const u8, shift: u8, allocator: *std.mem.Allocator) ![]u8 {
     var result = try allocator.alloc(u8, text.len);
@@ -33,23 +34,50 @@ pub fn decipher(text: []const u8, shift: u8, allocator: *std.mem.Allocator) ![]u
 }
 
 test "test cipher" {
-    var allocator = std.heap.page_allocator;
+    var allocator = testing.allocator;
 
     const text = "ABC";
 
     const result = try cipher(text, 2, &allocator);
     defer allocator.free(result);
 
-    try std.testing.expectEqualStrings(result, "CDE");
+    try testing.expectEqualStrings(result, "CDE");
 }
 
 test "test decipher" {
-    var allocator = std.heap.page_allocator;
+    var allocator = testing.allocator;
 
     const text = "CDE";
 
     const result = try decipher(text, 2, &allocator);
     defer allocator.free(result);
 
-    try std.testing.expectEqualStrings(result, "ABC");
+    try testing.expectEqualStrings(result, "ABC");
+}
+
+test "lower case to upper" {
+    var allocator = testing.allocator;
+
+    const result = try cipher("abc", 1, &allocator);
+    defer allocator.free(result);
+
+    try testing.expectEqualStrings(result, "BCD");
+}
+
+test "string with spaces works" {
+    var allocator = testing.allocator;
+
+    const result = try cipher("attack the castle", 1, &allocator);
+    defer allocator.free(result);
+
+    try testing.expectEqualStrings(result, "BUUBDL UIF DBTUMF");
+}
+
+test "rotates back to start" {
+    var allocator = testing.allocator;
+
+    const result = try cipher("z", 1, &allocator);
+    defer allocator.free(result);
+
+    try testing.expectEqualStrings(result, "A");
 }
