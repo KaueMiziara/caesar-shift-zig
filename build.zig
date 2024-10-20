0fc256de6597af9147fe8e5b34fd64e49f6ed7ea
KaueMiziara <kauemiziara@gmail.com>
2024-10-20 00:05:36 -0300

    refactor: move Caesar cipher related files to a new directory

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "caesar_shift",
-        .root_source_file = b.path("src/caesar.zig"),
+        .root_source_file = b.path("src/caesar/caesar.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -34,7 +34,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/caesar_tests.zig"),
+        .root_source_file = b.path("src/caesar/caesar_tests.zig"),
         .target = target,
         .optimize = optimize,
     });
